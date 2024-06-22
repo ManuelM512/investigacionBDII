b3963c7a9b4a52f3135e1872ee0268569e0ab35b
Passeriade <manuel5mongelos@gmail.com>
2024-06-22 13:28:04 -0300

    Docker, DB loader, csv files

---
+++ loader.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sqlalchemy import create_engine
+import os
+
+
+def connectionEngine(host, db, user, pw):
+    engine = create_engine(f"mysql://{user}:{pw}@{host}:3306/{db}")
+    return engine
+
+
+def loadDatabase(engine):
+    folder = "csv"
+    for filename in os.listdir(folder):
+        path = os.path.join(folder, filename)
+        print(path)
+        dataframe = pd.read_csv(path, encoding="ANSI")
+        dataframe.to_sql(filename[:-4], engine, if_exists="replace", index=False)
+
+
+def main():
+    engine = connectionEngine("localhost", "formulaUno", "root", "root")
+    loadDatabase(engine)
+
+
+main()
